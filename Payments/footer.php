c9301311470922798c12ac356462453b92f391e7
vhfs <vitorhugofs2018@gmail.com>
2022-06-10 10:43:18 -0300

    Inclusão inicial dos arquivos

---
+++ Payments/footer.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <link rel="stylesheet" type="text/css" href="css/footer.css">
+</head>
+<body>
+<footer class="rodape" >
+             <div class="alinhamento" >
+                <a href="index.html"><img class="rodape-img" src="img/pixlr-bg-result.png" alt="logo da Payments" ></a>
+            <div class="redes" >
+            <a href="https://www.tiktok.com/pt-BR/"><img class="tik-img" src="img/tiktok.png" alt="tik"></a>
+            <a href="https://www.instagram.com/"><img class="insta-img" src="img/insta.png" alt="insta"></a>
+            <a href="https://twitter.com/"><img class="tt-img" src="img/tt.png" alt="tt"></a>
+             </div>
+            </div>
+    
+         </footer>
+</body>
+</html>
